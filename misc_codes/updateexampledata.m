051dc94f51ea5e4b3b73ced9c120eb8d926da495
Deborah Kane <dkane@Deborahs-MacBook-Pro.local>
2015-03-16 15:14:24 -0700

    script for creating structure with necessary fields for input

---
+++ misc_codes/updateexampledata.m
@@ -0,0 +1,13 @@
+load ../../old_pickgui/exampledata/examplewfdata.mat
+
+% rename as needed
+for i=1:length(exampledata)
+    exampledata(i).dE = exampledata(i).vdataE;
+    exampledata(i).dN = exampledata(i).vdataN;
+    exampledata(i).dZ = exampledata(i).vdataZ;
+    
+    exampledata(i).distXYZ = (exampledata(i).stXYZ - exampledata(i).evXYZ)/1e3;
+end
+
+exampledata = rmfield(exampledata,{'vdataE','vdataN','vdataZ','evloc',...
+    'evXYZ','stloc','stXYZ','hypodist','AZ'});
