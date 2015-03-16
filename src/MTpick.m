3096a7488376738dfdf1adb9535c806b513c389a
Deborah Kane <dkane@Deborahs-MacBook-Pro.local>
2015-03-16 16:03:51 -0700

    now handles station selection

--- src/MTpick.m
+++ src/MTpick.m
@@ -28,90 +28,112 @@ pf_pick
 %% Step 1: Generate Fig 1, allow for selection of station from list. 
 %% Other content in Fig 1 is static until figure is updated.
 
+% %%%% Temporary approach: Loop over all stations in structure as in original code.
+% %%%% Even more temporary: Use a single station:
+% ista = 1;
+
+donewithsta=0;
+
+while donewithsta==0
+    % Step 1a,1b: Generate Fig 1, and allow for selection of station from list.
+    % Other content in Fig 1 is static until figure is updated.
+    f1 = gen_fig1(indata);
+    uiwait(f1);
 
 
-%%%% Temporary approach: Loop over all stations in structure as in original code.
-%%%% Even more temporary: Use a single station:
-ista = 1;
-
-
-
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Step 2: Run GUI for picking input event.
-%% Generate Figure 2: Waveforms
-
-% Rotate waveforms to radial-transverse directions
-[dH1,dH2,dZ] = rotatetoRTZ(indata(ista).dE,indata(ista).dN,indata(ista).dZ,indata(ista).BAZ);
-indata(ista).dR = dH1;
-indata(ista).dT = dH2;
-indata(ista).dZ = dZ;
-
-
-% Create synthetics
-createsynthetics
-
-% Create fields for later
-Nsta = length(indata);
-indata(Nsta).sta = nan;
-indata(Nsta).t_P = nan;
-indata(Nsta).t_nf = nan;
-indata(Nsta).t_S = nan;
-indata(Nsta).R_P = nan;
-indata(Nsta).R_nf = nan;
-indata(Nsta).R_S = nan;
-indata(Nsta).T_P = nan;
-indata(Nsta).T_nf = nan;
-indata(Nsta).T_S = nan;
-indata(Nsta).V_P = nan;
-indata(Nsta).V_nf = nan;
-indata(Nsta).V_S = nan;
-indata(Nsta).FLAG = [];
-indata(Nsta).COMMENT = [];
-indata(Nsta).FILTER = [];
-indata(Nsta).dumpR = [];
-indata(Nsta).dumpT = [];
-indata(Nsta).dumpZ = [];
+    % Step 1c: 
+    % Parse station choice in Fig 1.
+    temp = guidata(f1);
+    keyboard
+    if isfield(temp,'sta')==1
+        usesta = temp.sta;
+        ista = find(cellfun(@(x) ~isempty(strfind(x,char(usesta))),[indata.sta])==1);
+    end
+    
+    if isfield(temp,'done')==1
+        donewithsta = temp.done;
+    end
+    
+    
+    if donewithsta==1
+        return
+    end
 
 
 
 
-% Plot waveforms and synthetics 
-f2 = figure(2);
-ax(1) = subplot(3,1,1);
-plot(indata(ista).t,indata(ista).dR,'linewidth',2)
-hold on
-plot(tt,uh1)
-ylabel('Radial (m)', 'FontSize', 12)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Step 2: Run GUI for picking input event.
+    %% Generate Figure 2: Waveforms
 
-ax(2) = subplot(3,1,2);
-plot(indata(ista).t,indata(ista).dT,'linewidth',2)
-hold on
-plot(tt,uh2)
-ylabel('Transverse (m)', 'FontSize', 12)
-
-ax(3) = subplot(3,1,3);
-plot(indata(ista).t,indata(ista).dZ,'linewidth',2)
-hold on
-plot(tt,uz)
-xlabel('time (seconds)', 'FontSize',12)
-ylabel('Vertical (m)', 'FontSize', 12)
-
-linkaxes([ax(1) ax(2) ax(3)],'x')
+    % Rotate waveforms to radial-transverse directions
+    [dH1,dH2,dZ] = rotatetoRTZ(indata(ista).dE,indata(ista).dN,indata(ista).dZ,indata(ista).BAZ);
+    indata(ista).dR = dH1;
+    indata(ista).dT = dH2;
+    indata(ista).dZ = dZ;
 
 
+    % Create synthetics
+    createsynthetics
+
+    % Create fields for later
+    Nsta = length(indata);
+    indata(Nsta).sta = nan;
+    indata(Nsta).t_P = nan;
+    indata(Nsta).t_nf = nan;
+    indata(Nsta).t_S = nan;
+    indata(Nsta).R_P = nan;
+    indata(Nsta).R_nf = nan;
+    indata(Nsta).R_S = nan;
+    indata(Nsta).T_P = nan;
+    indata(Nsta).T_nf = nan;
+    indata(Nsta).T_S = nan;
+    indata(Nsta).V_P = nan;
+    indata(Nsta).V_nf = nan;
+    indata(Nsta).V_S = nan;
+    indata(Nsta).FLAG = [];
+    indata(Nsta).COMMENT = [];
+    indata(Nsta).FILTER = [];
+    indata(Nsta).dumpR = [];
+    indata(Nsta).dumpT = [];
+    indata(Nsta).dumpZ = [];
 
 
-%% Generate Figure 3 (in fig window 22): Initialize picking menu and pick for station
-pickphases(f2,indata(ista));
-uiwait(22);
 
 
-%% Save picks
-outdata = save_pickdata(f2,indata,guidata(22),ista);
+    % Plot waveforms and synthetics 
+    f2 = figure(2); clf;
+    ax(1) = subplot(3,1,1);
+    plot(indata(ista).t,indata(ista).dR,'linewidth',2)
+    hold on
+    plot(tt,uh1)
+    ylabel('Radial (m)', 'FontSize', 12)
+
+    ax(2) = subplot(3,1,2);
+    plot(indata(ista).t,indata(ista).dT,'linewidth',2)
+    hold on
+    plot(tt,uh2)
+    ylabel('Transverse (m)', 'FontSize', 12)
+
+    ax(3) = subplot(3,1,3);
+    plot(indata(ista).t,indata(ista).dZ,'linewidth',2)
+    hold on
+    plot(tt,uz)
+    xlabel('time (seconds)', 'FontSize',12)
+    ylabel('Vertical (m)', 'FontSize', 12)
+
+    linkaxes([ax(1) ax(2) ax(3)],'x')
 
 
 
+
+    %% Generate Figure 3 (in fig window 22): Initialize picking menu and pick for station
+    pickphases(f2,indata(ista));
+    uiwait(22);
+
+
+    %% Save picks
+    outdata = save_pickdata(f2,indata,guidata(22),ista);
+
+
+end
---
+++ src/gen_fig1.m
@@ -0,0 +1,93 @@
+function f1=gen_fig1(indata)
+% Preliminary attempt to create new control menu.
+% Note that output of this is automatically stored in variable: 
+% guidata(f1)
+
+
+f1 = figure(1);
+guidata(f1)
+set(f1,'Position',[1360,500,300,650])
+f1x = 3;
+f1y = 4;
+
+% Event metadata
+%subplot(f1x,f1y,[1 2])
+%text(0,0.5,'eventID')
+%text(0,0.3,'event Mw')
+%text(0,0.1,'Nsta')
+%axis off
+
+% Map of event and stations
+%subplot(f1x,f1y,[5 6 9 10])
+%hold on
+
+% mij solution info
+%subplot(f1x,f1y,[3 4])
+%text(0,0.5,'space for mij solution')
+%axis off
+
+% mij analysis
+%subplot(f1x,f1y,7)
+%text(0,0.5,'mij error analysis')
+%axis off
+
+% beachballs
+%subplot(f1x,f1y,8)
+%text(0,0.5,'future beachball here')
+
+
+
+% dropdown menu
+stalist = [indata.sta];
+%h1 = subplot(f1x,f1y,[11 12]);
+%set(h1,'Units','pixels')
+h_sta = uicontrol('Style','popupmenu',...
+			'String', stalist,...
+			'Position', [25,125,150,50],...
+			'Callback',{@select_STA})
+
+	function select_STA(h_sta,eventdata)		
+		ab = guidata(f1)
+		allsta = get(h_sta,'String')
+		ab.sta = allsta(get(h_sta,'Value'))
+		guidata(f1,ab)
+	end
+
+h_USESTA =  uicontrol('Style','pushbutton',...
+			'String','Select station','Position',[150,25,50,50],...
+			'Callback',{@select_USESTA});
+
+function select_USESTA(varargin)
+    uiresume(f1)
+    return
+end
+
+
+
+% finish with stations
+
+h_DONE =  uicontrol('Style','pushbutton',...
+			'String','DONE','Position',[225,25,50,50],...
+			'Callback',{@select_DONE});
+
+function select_DONE(varargin)
+    ab = guidata(f1)
+    ab.done = 1;
+    guidata(f1,ab)
+    uiresume(f1)
+    return
+end
+
+
+
+
+
+
+
+%uiresume(f1)
+%return
+	
+
+
+
+end
